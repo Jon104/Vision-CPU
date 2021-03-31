ff538a6551c906fff50b1d0a1b0e81834398ae6f
Jonathan Girard <jonathan.girard@celliersintelligents.com>
2021-03-30 22:31:50 -0400

    New data class - Added a loop to send "lots" of data

--- server.py
+++ server.py
@@ -35,41 +35,76 @@ from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer
 import numpy as np
 from array import array
 import json
- 
+import random
+import time
 
 PORTNUM = 8003
 rList = [1, 2, 3, 4]
 
-data = [
-  -0.99,0,
-  -0.98,0,
-  -0.97,0,
-  -0.851,0,
-  -0.85,0,
-  -0.849,0,
-  -0.76,0,
-  -0.75,0,
-  -0.749,0,
-  -0.747,0,
-  -0.746,0.5,
-  -0.745,0,
-  0.5,0,
-  0.73,0,
-  0.75,0,
-  0.749,0,
-  0.741,0,
-  0.74,0.5,
-  0.749,0.8,
-  0.9,0,
-  1.0,0,
-];
- 
+class Data:
+    def __init__(self, index):
+       self.index=index
+
+    def getData(self):  
+        chata = [
+            -0.99,0,
+            -0.98,0,
+            -0.97,0,
+            -0.851,0,
+            -0.85,0,
+            -0.849,0,
+            -0.76,0,
+            -0.75,0,
+            -0.749,0,
+            -0.747,0,
+            -0.746, 0.5,
+            -0.745,0,
+            -0.06,0,
+            -0.05,0.02,
+            -0.04,-0.13,
+            -0.03,0.27,
+            -0.02,-0.27,
+            -0.01,0.13,
+            0.0,-0.02,
+            0.01,0,
+            0.06,0,
+            0.07,0.05,
+            0.08,-0.2,
+            0.09,0.4,
+            0.1,-0.4,
+            0.11,0.15,
+            0.12,-0.05,
+            0.13,0,
+            0.5,0,
+            0.73,0,
+            0.75,0,
+            0.749,0,
+            0.741,0,
+            0.74,0.5,
+            0.749,0.8,
+            0.9,0,
+            1.0,0];
+
+        if (self.index % 2 == 1):
+            chata.append(0.4)
+        else:
+            chata.append(0.8)
+            
+        chata.append(1.0)
+        self.index = self.index + 1
+        return chata
+
+
+index = 1
 # Websocket class to echo received data
 class Echo(WebSocket):
-
     def handleMessage(self):
-        yop = json.dumps({"a": data})
-        self.sendMessage(yop.encode())
+        for x in range(1000):
+            global index
+            data=Data(index)
+            yop = json.dumps({"a": data.getData()})
+            self.sendMessage(yop.encode())
+            index = index + 1
  
     def handleConnected(self):
         print("Connected")
@@ -84,6 +119,7 @@ def close_server(signal, frame):
  
 if __name__ == "__main__":
     print("Websocket server on port %s" % PORTNUM)
+
     server = SimpleWebSocketServer('0.0.0.0', PORTNUM, Echo)
     print(server)
     signal.signal(signal.SIGINT, close_server)
